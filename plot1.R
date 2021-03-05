d1d51e38bbfd147eaf9a2100384372f076bb4fc7
hooman-cloud <farsani@asu.edu>
2021-03-05 08:00:39 -0500

    Fixed the use of png().

--- plot1.R
+++ plot1.R
@@ -6,8 +6,9 @@ data_raw$Date <- as.Date(data_raw$Date, format = "%d/%m/%Y")
 data_desired <- subset(data_raw, data_raw$Date == "2007-02-01" | data_raw$Date == "2007-02-02")
 
 data_desired$Global_active_power <- as.numeric(data_desired$Global_active_power)
+
+png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
 hist(data_desired$Global_active_power, breaks = 15, col = "red", ylim = c(0,1200), xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
-
-dev.copy(png, file="plot1.png", width=480, height=480, bg = "transparent")
+#dev.copy(png, file="plot1.png", width=480, height=480, bg = "transparent")
 dev.off()
